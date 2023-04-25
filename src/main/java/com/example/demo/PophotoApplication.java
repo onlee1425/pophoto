8136d75ad896606070875c5749159d2addb01cea
onlee1425 <enflenffl@gmail.com>
2023-04-25 18:52:52 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/PophotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PophotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PophotoApplication.class, args);
+	}
+
+}
